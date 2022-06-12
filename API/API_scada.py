1f4af0e9d8554bebdbc8347c186e4caa4377b157
Miguel Ángel Álvarez <michel_civic@hotmail.com>
2022-06-12 20:03:47 +0200

    Funciona envio desde PLC a API y se visualiza en GUI: Necesario separar API de GUI, ventaja: comunicacion PLC-API siempre y cuando se desee se abre la GUI

--- API/API_scada.py
+++ API/API_scada.py
@@ -1,41 +1,35 @@
-import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem,QHeaderView
-from PyQt5.QtGui import QIcon, QPixmap
+
 import requests
 import json
 from time import sleep
 from datetime import datetime
-from ObjectListView import ObjectListView, ColumnDefn
 from typing import Optional   
 from fastapi import FastAPI, Request
 from pydantic import BaseModel  
 from datetime import datetime
 from threading import Thread
-from time import sleep
 import uvicorn
 from API_gestor_datos import Conexion
 import asyncio
 import codecs
 
+## CLASE PARA RECIBIR DATOS PLC ##
 class Envio_PLC(BaseModel):         
     envio: bytes
+## CLASE CREACION GUI (INTERFAZ GRAFICA EN PYQT5) ##
 
-class gui_api_scada(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("API_GUI.ui",self)
-        ## LOGO ##
-        #pixmap = QPixmap('Letras_gestor_almacen_2.png')
-        #self.img_letras_logo.setPixmap(pixmap)
-        ## FONDO ##
-        #pixmap = QPixmap('Fondo_gestor_almacen.png')
-        #self.img_fondo.setPixmap(pixmap)
 
+
+    
 ## CREACION API ##
 api_scada = FastAPI() 
+
+## HEREDAR CLASE DE LA BASE DE DATOS (BdD) ##
 conexion = Conexion()
 
+## VARIABLES ##
+mensaje_plc = dict
+codigo_inicial = 1000000    
 
 ## ENVIOS ##
 @api_scada.get("/envios")
@@ -43,27 +37,35 @@ def get_envios():
     list_envio = conexion.mostrar_envios()
     return list_envio
 
-def bytes_to_str(bytes1):
-    return bytes1.decode('utf-8')
-
 @api_scada.post("/envios")
 async def post_envios(requests: Request):
     mensaje_bytes = await requests.body()
     print(f'Mensaje PLC original: {mensaje_bytes}, tipo:{type(mensaje_bytes)}')
     mensaje_str = bytes_to_str(mensaje_bytes)
     mensaje_separado = list(mensaje_str)
-    n_piezas = int.from_bytes(mensaje_separado[0].encode('ascii'), byteorder='little')
-    n_cajas = int.from_bytes(mensaje_separado[1].encode('ascii'), byteorder='little')
-    n_pales = int.from_bytes(mensaje_separado[2].encode('ascii'), byteorder='little')
-    n_marcha = int.from_bytes(mensaje_separado[3].encode('ascii'), byteorder='little')
-    mensaje_plc = dict(
-        piezas = n_piezas,
-        cajas = n_cajas,
-        pales = n_pales,
-        marcha = n_marcha,
-    )
+    print(mensaje_separado)
+    valores_mensaje = []
+    for i in range(0,len(mensaje_separado)):
+        if mensaje_separado[i] != 'p':
+            valores_mensaje.append(int.from_bytes(mensaje_separado[i].encode('ascii'), byteorder='little'))
+        else:
+            valores_mensaje.append(0)    
+    creacion_codigo = codigo_inicial + 1
+    if len(mensaje_separado)==4:
+        mensaje_plc = dict(
+            codigo = creacion_codigo,
+            piezas = valores_mensaje[0],
+            cajas = valores_mensaje[1],
+            pales = valores_mensaje[2],
+            marcha = valores_mensaje[3],
+        )
+
     print(f'Datos enviados del PLC a la BdD Envios: {mensaje_plc}')
     conexion.insertar_envios(mensaje_plc)
+        
+        
+    if mensaje_plc['marcha']==1:
+        visualizar_tabla()
     return 'Datos recibidos OK'
 
 @api_scada.delete("/envios")
@@ -71,18 +73,24 @@ def delete_envios():
     conexion.eliminar_envios()
     return 'Eliminado completo Envios OK'
 
-def run_server():
-    uvicorn.run(api_scada, host="0.0.0.0", port=8000)
-def run_gui():
-    app = QApplication(sys.argv)
-    GUI = gui_api_scada()
-    GUI.show()
-    sys.exit(app.exec_()) 
 
+## FUNCIONES ##
+
+def bytes_to_str(bytes1):
+    return bytes1.decode('utf-8')
+
+## FECHA ACTUAL ##
+def fecha_actual():
+    ahora = datetime.now()
+    formato = "%Y-%m-%d_%H:%M:%S"
+    fecha_hora_actual = ahora.strftime(formato)
+    return fecha_hora_actual
+
+
+    
 if __name__ == '__main__':
-    chequeo = Thread(target=run_server, daemon=True)
-    chequeo.start()
-    run_gui()
+    uvicorn.run(api_scada, host="0.0.0.0", port=8000)
+    
     
 
      
---
+++ GUI/GUI_API.py
@@ -0,0 +1,108 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem,QHeaderView
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from ObjectListView import ObjectListView, ColumnDefn
+import requests
+import json
+from time import sleep
+from datetime import datetime
+import subprocess
+from threading import Thread
+class gui_api_scada(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("API_GUI.ui",self)
+        ## LOGO ##
+        #pixmap = QPixmap('Letras_API_scada.png')
+        #self.img_letras_logo.setPixmap(pixmap)
+        ## FONDO ##
+        pixmap = QPixmap('Fondo_API_scada.png')
+        self.img_fondo.setPixmap(pixmap)
+        self.boton_refrescar.clicked.connect(self.refresco_auto_estado)
+        
+
+        ## TABLA ##    
+        self.table.setColumnCount(4)
+        self.table.setRowCount(50)
+        self.table.setHorizontalHeaderLabels(['Codigo','Piezas','Cajas','Pales'])
+        self.table.horizontalHeader().setSectionResizeMode(80)
+        self.table.setItem(1,1,QTableWidgetItem('HOLA'))
+
+        ## VARIABLES AUXILIARES ##
+        self.ip_api= 'http://localhost:8000/'
+        self.n_piezas_total = int
+        self.n_cajas_total = int
+        self.n_pales_total = int
+    ## ENVIAR A TABLA ##
+    def visualizar_tabla(self):
+        all_envios = requests.get(f'{self.ip_api}envios')
+        json_all_envios = all_envios.json()
+        list_json_all_envios = [dict(id_item) for id_item in json_all_envios]
+        print(f'BdD Envios: {list_json_all_envios}')
+        inv_codigo =  []
+        inv_piezas = []
+        inv_cajas = []
+        inv_pales = []
+            
+        for i in range(0,len(list_json_all_envios)):
+            dict_json_all_envio = list_json_all_envios[i]
+            valor_codigo = dict_json_all_envio['codigo']
+            valor_piezas = dict_json_all_envio['piezas']
+            valor_cajas = dict_json_all_envio['cajas']
+            valor_pales = dict_json_all_envio['pales']
+
+            inv_codigo.append(valor_codigo)
+            inv_piezas.append(valor_piezas)
+            inv_cajas.append(valor_cajas)
+            inv_pales.append(valor_pales)
+
+        inventario = dict(
+            Inv_Codigo = inv_codigo,
+            Inv_Piezas = inv_piezas,
+            Inv_Cajas = inv_cajas,
+            Inv_Pales = inv_pales,
+        )
+        print(f'Inventario para mostrar: {inventario}')
+        self.table.clear()
+        self.table.setHorizontalHeaderLabels(['Codigo','Piezas','Cajas','Pales'])
+        for n, key in enumerate(inventario.keys()):
+            for m, item in enumerate(inventario[key]):
+                self.table.setItem(m,n,QTableWidgetItem(str(item)))
+        self.table.verticalHeader().setDefaultSectionSize(80)
+        list_id = []
+        for i in range(0,len(list_json_all_envios)):
+            dict_json_all_envios_id = list_json_all_envios[i]
+            n_id = dict_json_all_envios_id['id']
+            list_id.append(n_id)
+        n_id_max = max(list_id)
+        for i in range(0,len(list_json_all_envios)):
+            dict_json_all_envios_id_max = list_json_all_envios[i]
+            if dict_json_all_envios_id_max['id']==n_id_max:
+                n_piezas_id_max = dict_json_all_envios_id_max['piezas']
+                n_cajas_id_max = dict_json_all_envios_id_max['cajas']
+                n_pales_id_max = dict_json_all_envios_id_max['pales']
+
+        self.n_piezas_total = (n_piezas_id_max) + (4 * (n_pales_id_max-1))
+        self.n_cajas_total = (n_cajas_id_max) + (4 * (n_pales_id_max-1))
+        self.n_pales_total = n_pales_id_max - 1
+        self.in_n_piezas.setText(str(self.n_piezas_total))
+        self.in_n_cajas.setText(str(self.n_cajas_total))
+        self.in_n_pales.setText(str(self.n_pales_total))
+        
+    def refresco_auto_estado(self):
+        self.visualizar_tabla()
+        
+
+
+def run_gui():
+    app = QApplication(sys.argv)
+    GUI = gui_api_scada()
+    GUI.show()
+    sys.exit(app.exec_()) 
+
+if __name__ == '__main__':
+    run_gui()
+    
